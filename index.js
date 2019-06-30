8622737111b60d02f178d3463a448741916667a4
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-06-30 20:57:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ function happyHolidaysTo(name){
 happyHolidaysTo("Rusty");
 
 function happyHolidayTo(holiday, name){
-  
+  return `Happy ${holiday}, ${name}!`;
 }
+
+happyHolidayTo("Summer Solstice", "Krystek");
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holidays}` ;
+}
+
+holidayCountdown("Gabriel's birthday", 12);
